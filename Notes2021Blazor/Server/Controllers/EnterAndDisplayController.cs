f2b3a1e968fce37b9e3b1a004c8b40bb8b0b3775
Dale Sinder <sinder@illinois.edu>
2020-02-01 17:54:04 -0600

    Direct display of note

---
+++ Notes2021Blazor/Server/Controllers/EnterAndDisplayController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Notes2021Blazor.Shared;
+
+namespace Notes2021Blazor.Server.Controllers
+{
+    [Authorize(Roles = "User")]
+    [Route("api/[controller]/{noteId}")]
+    [ApiController]
+    public class EnterAndDisplayController : ControllerBase
+    {
+        private readonly NotesDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public EnterAndDisplayController(NotesDbContext db,
+                UserManager<IdentityUser> userManager
+                )
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<NoteHeader> Get(string noteId)
+        {
+            long Id = long.Parse(noteId);
+
+            NoteHeader header = await _db.NoteHeader.Where(p => p.Id == Id).FirstOrDefaultAsync();
+
+            return header;
+        }
+
+    }
+}
