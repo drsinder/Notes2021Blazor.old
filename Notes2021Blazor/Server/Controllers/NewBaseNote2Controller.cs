207d827c3d9848a389c54eccbe6ec5af6fd7d8d6
Dale Sinder <sinder@illinois.edu>
2020-02-02 03:22:18 -0600

    Show note after written.

---
+++ Notes2021Blazor/Server/Controllers/NewBaseNote2Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Notes2021Blazor.Shared;
+
+namespace Notes2021Blazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewBaseNote2Controller : ControllerBase
+    {
+        private readonly NotesDbContext _db;
+
+        public NewBaseNote2Controller(NotesDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<NoteHeader> Get()
+        {
+            NoteHeader nh = _db.NoteHeader.OrderByDescending(p => p.Id).FirstOrDefault();
+            return nh;
+        }
+    }
+}
