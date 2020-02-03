62d2055f1a91cbca1bc6782a48c17f7f5a6c01b6
Dale Sinder <sinder@illinois.edu>
2020-02-02 22:50:52 -0600

    Tweak who can edit/delete notes

--- Notes2021Blazor/Shared/NotesModels/NoteDisplayIndexModel.cs
+++ Notes2021Blazor/Shared/NotesModels/NoteDisplayIndexModel.cs
@@ -7,7 +7,7 @@ namespace Notes2021Blazor.Shared
         public NoteFile noteFile { get; set; }
         public int ArcId { get; set; }
 
-        public int NoteCount { get; set; }
+        //public int NoteCount { get; set; }
         public NoteAccess myAccess { get; set; }
         public bool isMarked { get; set; }
         public string rPath { get; set; }
@@ -15,10 +15,11 @@ namespace Notes2021Blazor.Shared
         public int ExpandOrdinal { get; set; }
         public List<NoteHeader> Notes { get; set; }
 
+        public NoteHeader myHeader { get; set; }
 
-        public List<string> Cheaders { get; set; }
-        public List<string> Lheaders { get; set; }
-        public string panelEnd { get; set; }
+        //public List<string> Cheaders { get; set; }
+        //public List<string> Lheaders { get; set; }
+        //public string panelEnd { get; set; }
         public TZone tZone { get; set; }
         public List<NoteHeader> AllNotes { get; set; }
         public string linkedText { get; set; }
