456f0711e4ae7db9f911d89933ea4baf8c44fd34
Dale Sinder <sinder@illinois.edu>
2020-02-04 10:04:24 -0600

    Scroll index entry into veiw when returning from reading

--- Notes2021Blazor/Shared/NotesModels/NoteDisplayIndexModel.cs
+++ Notes2021Blazor/Shared/NotesModels/NoteDisplayIndexModel.cs
@@ -6,20 +6,13 @@ namespace Notes2021Blazor.Shared
     {
         public NoteFile noteFile { get; set; }
         public int ArcId { get; set; }
-
-        //public int NoteCount { get; set; }
         public NoteAccess myAccess { get; set; }
         public bool isMarked { get; set; }
         public string rPath { get; set; }
         public string scroller { get; set; }
         public int ExpandOrdinal { get; set; }
         public List<NoteHeader> Notes { get; set; }
-
         public NoteHeader myHeader { get; set; }
-
-        //public List<string> Cheaders { get; set; }
-        //public List<string> Lheaders { get; set; }
-        //public string panelEnd { get; set; }
         public TZone tZone { get; set; }
         public List<NoteHeader> AllNotes { get; set; }
         public string linkedText { get; set; }
