780bcf78a76ac2d47d55b74127a219af448c9d94
Dale Sinder <sinder@illinois.edu>
2020-02-05 18:45:20 -0600

    Forward a note

---
+++ Notes2021Blazor/Server/Controllers/ForwardController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Notes2021Blazor.Server.Services;
+using Notes2021Blazor.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes2021Blazor.Server.Controllers
+{
+    [Authorize(Roles = "User")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ForwardController : ControllerBase
+    {
+        private readonly NotesDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ForwardController(NotesDbContext db,
+            UserManager<IdentityUser> userManager
+          )
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task Post(ForwardViewModel fv)
+        {
+            NoteHeader nh = await NoteDataManager.GetBaseNoteHeaderById(_db, fv.NoteID);
+
+            IdentityUser usr = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            UserData ud = await _db.UserData.SingleOrDefaultAsync(p => p.UserId == usr.Id);
+
+            string myEmail = await LocalService.MakeNoteForEmail(fv, _db, User.Identity.Name, ud.DisplayName);
+
+            EmailSender emailSender = new EmailSender();
+
+            await emailSender.SendEmailAsync(usr.UserName, fv.NoteSubject, myEmail);
+        }
+
+    }
+}
---
+++ Notes2021Blazor/Server/Services/LocalService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes2021Blazor.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes2021Blazor.Server.Services
+{
+    public static class LocalService
+    {
+        public static async Task<string> MakeNoteForEmail(ForwardViewModel fv, NotesDbContext db, string email, string name)
+        {
+            NoteHeader nc = await NoteDataManager.GetNoteByIdWithFile(db, fv.NoteID);
+
+            if (!fv.hasstring || !fv.wholestring)
+            {
+                return "Forwarded by Notes 2021 - User: " + email + " / " + name
+                    + "<p>File: " + nc.NoteFile.NoteFileName + " - File Title: " + nc.NoteFile.NoteFileTitle + "</p><hr/>"
+                    + "<p>Author: " + nc.AuthorName + "  - Director Message: " + nc.NoteContent.DirectorMessage + "</p><p>"
+                    + "<p>Subject: " + nc.NoteSubject + "</p>"
+                    + nc.LastEdited.ToShortDateString() + " " + nc.LastEdited.ToShortTimeString() + " UTC" + "</p>"
+                    + nc.NoteContent.NoteBody
+                    + "<hr/>" + "<a href=\"" + Globals.ProductionUrl + "NoteDisplay/Display/" + fv.NoteID + "\" >Link to note</a>";
+            }
+            else
+            {
+                List<NoteHeader> bnhl = await db.NoteHeader
+                    .Where(p => p.NoteFileId == nc.NoteFileId && p.NoteOrdinal == nc.NoteOrdinal && p.ResponseOrdinal == 0)
+                    .ToListAsync();
+                NoteHeader bnh = bnhl[0];
+                fv.NoteSubject = bnh.NoteSubject;
+                List<NoteHeader> notes = await db.NoteHeader.Include("NoteContent")
+                    .Where(p => p.NoteFileId == nc.NoteFileId && p.NoteOrdinal == nc.NoteOrdinal)
+                    .ToListAsync();
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append("Forwarded by Notes 2020 - User: " + email + " / " + name
+                    + "<p>\nFile: " + nc.NoteFile.NoteFileName + " - File Title: " + nc.NoteFile.NoteFileTitle + "</p>"
+                    + "<hr/>");
+
+                for (int i = 0; i < notes.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        sb.Append("<p>Base Note - " + (notes.Count - 1) + " Response(s)</p>");
+                    }
+                    else
+                    {
+                        sb.Append("<hr/><p>Response - " + notes[i].ResponseOrdinal + " of " + (notes.Count - 1) + "</p>");
+                    }
+                    sb.Append("<p>Author: " + notes[i].AuthorName + "  - Director Message: " + notes[i].NoteContent.DirectorMessage + "</p>");
+                    sb.Append("<p>Subject: " + notes[i].NoteSubject + "</p>");
+                    sb.Append("<p>" + notes[i].LastEdited.ToShortDateString() + " " + notes[i].LastEdited.ToShortTimeString() + " UTC" + " </p>");
+                    sb.Append(notes[i].NoteContent.NoteBody);
+                    sb.Append("<hr/>");
+                    sb.Append("<a href=\"");
+                    sb.Append(Globals.ProductionUrl + "NoteDisplay/Display/" + notes[i].Id + "\" >Link to note</a>");
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
