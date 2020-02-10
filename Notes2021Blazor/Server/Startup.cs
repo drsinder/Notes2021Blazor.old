83149bc0f774a19b4d414d17d6b80f6d256a337e
Dale Sinder <sinder@illinois.edu>
2020-02-10 17:58:15 -0600

    Customize <app> ssection with prettier stuff

--- Notes2021Blazor/Server/Startup.cs
+++ Notes2021Blazor/Server/Startup.cs
@@ -118,7 +118,7 @@ namespace Notes2021Blazor.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UsePathBase("/Notes2021Blazor");
+            app.UsePathBase(Globals.PathBase);
 
             UpdateDatabase(app);
 
