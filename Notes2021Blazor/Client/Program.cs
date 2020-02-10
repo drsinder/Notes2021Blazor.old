1b66ee17f66f039c8f4e6e04a66bfe7e544af1fe
Dale Sinder <sinder@illinois.edu>
2020-02-10 08:41:26 -0600

    Update to Blazor 3.2 preview 1.

--- Notes2021Blazor/Client/Program.cs
+++ Notes2021Blazor/Client/Program.cs
@@ -1,16 +1,27 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Blazored.Modal;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Notes2021Blazor.Client
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            builder.Services.AddOptions();
+            builder.Services.AddBlazoredModal();
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+            builder.Services.AddScoped<IAuthService, AuthService>();
+
+            await builder.Build().RunAsync();
+        }
     }
 }
